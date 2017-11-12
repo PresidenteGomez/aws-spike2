a587ca5b3375a9425f76e4da6b8b5e450b6832a0
Nestor Gomez Jimenez <gomez315@umn.edu>
2017-11-11 18:35:16 -0600

    starting out with some heat

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var Routes = require('./server/routes');
+var config = require('./server/config/config');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+/** load routes*/
+
+require('./server/routes')(app);
+
+var port = config.server.port;
+
+app.listen(process.env.PORT || port);
+
+console.log('App started on port ' + port);
