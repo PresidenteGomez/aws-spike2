7bbb76b79a7c76b8702a88d2f2317d2137710389
Nestor Gomez Jimenez <gomez315@umn.edu>
2017-11-12 15:05:05 -0600

    chgd server.js structure. No need for config.js anymore

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var Routes = require('./server/routes');
-var config = require('./server/config/config');
+// var config = require('./server/config/config');
 var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var port = process.env.PORT || 3000;
 
 /** load routes*/
 
 require('./server/routes')(app);
 
-var port = config.server.port;
+// var port = config.server.port;
 
 app.listen(process.env.PORT || port);
 
