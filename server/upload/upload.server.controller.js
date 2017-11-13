c6a0d4450cfeac55bc80fa38a6d2c14f1a118808
Nestor Gomez Jimenez <gomez315@umn.edu>
2017-11-12 18:22:24 -0600

    styling

--- server/upload/upload.server.controller.js
+++ server/upload/upload.server.controller.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk')
 const async = require('async')
-const bucketName = "grownorthspike"
+const bucketName = 'grownorthspike'
 const path = require('path')
 const fs = require('fs')
 let pathParams, image, imageName;
@@ -8,69 +8,82 @@ let pathParams, image, imageName;
 AWS.config.loadFromPath('config.json')
 
 
-const s3 = new AWS.S3({region: 'us-west-2'})
+const s3 = new AWS.S3({ region: 'us-west-2' })
 const createMainBucket = (callback) => {
 	// Create the parameters for calling createBucket
 	const bucketParams = {
-	   Bucket : bucketName
-	};                    
-	s3.headBucket(bucketParams, function(err, data) {
-	   if (err) {
-	   	console.log("ErrorHeadBucket", err)
-	      	s3.createBucket(bucketParams, function(err, data) {
-			   if (err) {
-			   	console.log("Error", err)
-			      callback(err, null)
-			   } else {
-			      callback(null, data)
-			   }
+		Bucket: bucketName
+	};
+	s3.headBucket(bucketParams, function (err, data) {
+		if (err) {
+			console.log('ErrorHeadBucket', err)
+			s3.createBucket(bucketParams, function (err, data) {
+				if (err) {
+					console.log('Error', err)
+					callback(err, null)
+				} else {
+					callback(null, data)
+				}
 			});
-	   } else {
-	      callback(null, data)
-	   }
-	})                             
+		} else {
+			callback(null, data)
+		}
+	})
 }
 
 const createItemObject = (callback) => {
-  const params = { 
-        Bucket: bucketName, 
-        Key: `${imageName}`, 
-        ACL: 'public-read',
-        Body:image
-    };
-	s3.putObject(params, function (err, data) {
+	const params = {
+		Bucket: bucketName,
+		Key: `${imageName}`,
+		ACL: 'public-read',
+		Body: image,
+		ContentEncoding: 'base64',
+		ContentType: 'image/png'
+	};
+	s3.putObject(params, function (err, data, URL) {
 		if (err) {
-	    	console.log("Error uploading image: ", err);
-	    	callback(err, null)
-	    } else {
-	    	console.log("Successfully uploaded image on S3", data);
-	    	callback(null, data)
-	    }
-	})  
-}
+			console.log("Error uploading image: ", err)
+			callback(err, null)
+		} else {
+			console.log("Successfully uploaded image on S3", data)
+			callback(null, data)
+		}
+	});
+};
 exports.upload = (req, res, next) => {
 	var tmp_path = req.files.file.path;
-    // console.log("item", req.files.file)
+	// console.log("item", req.files.file)
 	var tmp_path = req.files.file.path;
 	image = fs.createReadStream(tmp_path);
-    imageName = req.files.file.name;
-    async.series([
-        createMainBucket,
-        createItemObject
-        ], (err, result) => {
-        if(err) return res.send(err)
-        else return res.json({message: "Successfully uploaded"}) 
-    })
+	imageName = req.files.file.name;
+	async.series([
+		createMainBucket,
+		createItemObject
+	], (err, result) => {
+		if (err) return res.send(err)
+		else return res.redirect('back')
+	})
 }
 exports.displayForm = (req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/html"
-    });
-    res.write(
-        '<form action="/upload" method="post" enctype="multipart/form-data">' +
-        '<input type="file" name="file">' +
-        '<input type="submit" value="Upload">' +
-        '</form>'
-    );
-    res.end();
+	res.writeHead(200, {
+		"Content-Type": "text/html"
+	});
+	res.write(
+		'<style> body {background-color: linen; } h1 {color: maroon; margin: 40px;} </style >'+
+		'<h1> AWS Image Upload </h1>' +
+		'<h3> Upload your Image: </h3>' +
+		'<form action="/upload" method="post" id="uploadImage" enctype="multipart/form-data">' +
+		'<input type="file" name="file">' +
+		'<input type="submit" value="Upload">' +
+		'</form>'
+
+
+	);
+
+	res.end();
+
+	// s3.getElementById(id = "uploadImage").onsubmit = function () {
+	// 	location.reload(true);
+	// };
 };
+
